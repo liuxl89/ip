e86c5cb797e3d2cd6b4f6c686574e6143134b488
Xinle Sheila Liu <liuxl89@gmail.com>
2019-01-13 16:21:30 -0800

    Fix typo in script.

--- constraints_for_cardiology.m
+++ constraints_for_cardiology.m
@@ -1,11 +1,11 @@
 % Assuming M_{pt} = M_{p(am, pm)}.
 
 % They are all generic (equality & inequality) constraints.
-%  - cadiology_tasks, cadiology_people: relatively independent.
+%  - cardiology_tasks, cardiology_people: relatively independent.
 
 
 function [mat_a, vec_b, mat_a_le, vec_b_le, ub] = constraints_for_cardiology(
-    cadiology_tasks, cadiology_people)
+    cardiology_tasks, cardiology_people)
   global NUM_PEOPLE;
   global NUM_TASKS;
   lb = zeros(NUM_PEOPLE, NUM_TASKS);
@@ -18,8 +18,8 @@ function [mat_a, vec_b, mat_a_le, vec_b_le, ub] = constraints_for_cardiology(
 
   % Dedicated and backup cardiology people: no other people should be assigned.
   ub = ones(size(lb));
-  non_cadiology_people = setdiff(1 : NUM_PEOPLE, cadiology_people);
-  ub(non_cadiology_people, cadiology_tasks) = 0;
+  non_cardiology_people = setdiff(1 : NUM_PEOPLE, cardiology_people);
+  ub(non_cardiology_people, cardiology_tasks) = 0;
   ub = ub(:);
 
 endfunction
